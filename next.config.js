907ce54bde6beaf81d2e56c4c4ad8fc644cfb686
Sadanand_gadwal <Sadanandgadwal@gmail.com>
2023-06-05 13:55:55 +0530

    code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["i.redd.it", "www.reddit.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
